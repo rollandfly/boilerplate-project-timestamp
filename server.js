5a77986389aef4377c0ff0421ad63cf4e56975ee
damien ventura <>
2021-04-26 11:27:58 +0000

    API timestamp

--- server.js
+++ server.js
@@ -25,6 +25,30 @@ app.get("/api/hello", function (req, res) {
 });
 
 
+//API timestamp endpoint
+/*app.get("/api/timestamp/:date_string", (req, res) => {
+  res.json({ unix: Date.now(), utc: Date() });
+});*/
+
+app.get("/api/:date_string", (req, res) => {
+  let dateString = req.params.date_string;
+
+  //A 4 digit number is a valid ISO-8601 for the beginning of that year
+  //5 digits or more must be a unix time, until we reach a year 10,000 problem
+  if (/\d{5,}/.test(dateString)) {
+    const dateInt = parseInt(dateString);
+    //Date regards numbers as unix timestamps, strings are processed differently
+    res.json({ unix: dateInt, utc: new Date(dateInt).toUTCString() });
+  } else {
+    let dateObject = new Date(dateString);
+
+    if (dateObject.toString() === "Invalid Date") {
+      res.json({ error: "Invalid Date" });
+    } else {
+      res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+    }
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
